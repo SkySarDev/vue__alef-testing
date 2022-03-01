43b5cd55e7c1af351172c0797cf88e141d885fe6
SkySarDev <skysar@ya.ru>
2022-03-01 12:11:08 +0300

    feat(ui): add ui component input

---
+++ src/components/UI/MainInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>{{ label }}</div>
+  <input
+    :type="type"
+    v-model="modelValue"
+    @input="changeValue"
+    required
+  />
+</template>
+
+<script>
+export default {
+  name: "MainInput",
+  props: {
+    type: {
+      type: String,
+      defaultValue: "text",
+      required: false,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    modelValue: [String, Number],
+  },
+  methods: {
+    changeValue(e) {
+      this.$emit("update:modelValue", e.target.value);
+    },
+  },
+  inheritAttrs: false
+};
+</script>
+
+<style scoped>
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  input[type="number"] {
+    -moz-appearance: textfield;
+  }
+</style>
