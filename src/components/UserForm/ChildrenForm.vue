4a50e0f7fb919f3c0cf1fb432059a50925317f1f
SkySarDev <skysar@ya.ru>
2022-03-01 20:24:09 +0300

    feat: add animations for button addChildren and list of children

--- src/components/UserForm/ChildrenForm.vue
+++ src/components/UserForm/ChildrenForm.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="form-header">
     <h2 class="block-title">Дети (макс. 5)</h2>
-    <main-button
-      v-if="userData.children.length < 5"
-      @click="addChildren"
-      variant="outlined"
-      text="Добавить ребенка"
-    />
+    <transition name="button-animation">
+      <main-button
+        v-if="userData.children.length < 5"
+        @click="addChildren"
+        variant="outlined"
+        text="Добавить ребенка"
+        type="button"
+      />
+    </transition>
   </div>
 
   <div class="form-field">
-     <child-item
-       v-for="child in userData.children"
-       :key="child.id"
-       :child="child"
-       :delete-children="deleteChildren"
-     />
+    <transition-group name="list-animation">
+      <child-item
+        v-for="child in userData.children"
+        :key="child.id"
+        :child="child"
+        :delete-children="deleteChildren"
+      />
+    </transition-group>
   </div>
 </template>
 
@@ -55,5 +60,25 @@ export default {
 
 .form-field {
   margin-top: 10px;
+}
+
+.button-animation-enter-active,
+.button-animation-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.button-animation-enter-from,
+.button-animation-leave-to {
+  opacity: 0;
+}
+
+.list-animation-enter-active,
+.list-animation-leave-active {
+  transition: all 0.5s ease;
+}
+.list-animation-enter-from,
+.list-animation-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
 }
 </style>
